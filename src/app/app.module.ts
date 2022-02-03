baaacb9234be4c6c85c3454a6cecb8cce3ef7815
Armenuhi97 <armenuhimanukyan@annaniks.com>
2022-02-03 19:10:13 +0400

    create structural and attribute directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShowElementDirective } from './directives/show-element.directive';
+import { ChangeBackgroundColorDirective } from './directives/change-background-color.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowElementDirective,
+    ChangeBackgroundColorDirective
   ],
   imports: [
     BrowserModule
